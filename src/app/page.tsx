027b689d6ad5dca12095ecbe02dc7a8a9fbf282c
Viktar Rudzenia <viktarrudzenia@gmail.com>
2024-08-31 16:25:09 +0200

    feat: added background image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <div className={styles.wrapper}>It is a home page for Rick and Morty</div>;
+  return <div className={styles.wrapper} />;
 }
